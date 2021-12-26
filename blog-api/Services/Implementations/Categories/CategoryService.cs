8229001e5853dd9ba713a53855fba3cb574c6afd
Saverio-Angelicola <angelicola.saverio@gmail.com>
2021-12-26 10:59:38 +0100

    rename dependencires that are injected

--- blog-api/Services/Implementations/Categories/CategoryService.cs
+++ blog-api/Services/Implementations/Categories/CategoryService.cs
@@ -6,40 +6,40 @@ namespace blog_api.Services.Implementations.Categories
 {
     public class CategoryService : ICategoryService
     {
-        private readonly ICategoryRepository _categoryRepository;
-        public CategoryService(ICategoryRepository repository)
+        private readonly ICategoryRepository categoryRepository;
+        public CategoryService(ICategoryRepository categoryRepository)
         {
-            this._categoryRepository = repository;
+            this.categoryRepository = categoryRepository;
         }
 
         public async Task<Category> Add(string name)
         {
             Category category = new(name);
-            return await _categoryRepository.Create(category);
+            return await categoryRepository.Create(category);
         }
 
         public async Task<Category> Update(int id, string name)
         {
-            Category updatedCategory = _categoryRepository.FindById(id);
+            Category updatedCategory = categoryRepository.FindById(id);
             updatedCategory.Name = name;
-            return await _categoryRepository.Update(updatedCategory);
+            return await categoryRepository.Update(updatedCategory);
         }
 
         public async Task<Category> Delete(int id)
         {
-            Category deletedCategory = _categoryRepository.FindById(id);
-            return await _categoryRepository.Delete(deletedCategory);
+            Category deletedCategory = categoryRepository.FindById(id);
+            return await categoryRepository.Delete(deletedCategory);
         }
 
         public List<Category> GetAllCategories()
         {
-            List<Category> categories = _categoryRepository.FindAll();
+            List<Category> categories = categoryRepository.FindAll();
             return categories;
         }
 
         public Category GetCategoryById(int id)
         {
-            Category category = _categoryRepository.FindById(id);
+            Category category = categoryRepository.FindById(id);
             return category;
         }
     }
