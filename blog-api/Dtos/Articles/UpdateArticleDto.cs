aa187d7453ed72ff6d442f387a5285f0b1aebbab
Saverio-Angelicola <angelicola.saverio@gmail.com>
2021-12-26 11:30:33 +0100

    add empty constructor

--- blog-api/Dtos/Articles/UpdateArticleDto.cs
+++ blog-api/Dtos/Articles/UpdateArticleDto.cs
@@ -5,6 +5,14 @@
         public string? Title { get; set; }
         public string? Content { get; set; }
         public int? CategoryId { get; set; }
+
+        public UpdateArticleDto()
+        {
+            Title = string.Empty;
+            Content = string.Empty;
+            CategoryId = null;
+        }
+
         public UpdateArticleDto(string? title, string? content, int? categoryId)
         {
             Title = title;
