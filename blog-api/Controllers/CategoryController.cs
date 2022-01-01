040e2ce0ebf99aeae7866862514f2371119f899b
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-01-01 09:21:47 +0100

    return Ok on UpdateCategory Method

--- blog-api/Controllers/CategoryController.cs
+++ blog-api/Controllers/CategoryController.cs
@@ -48,7 +48,7 @@ namespace blog_api.Controllers
         {
             try
             {
-                return await categoryService.Update(id, category.Name);
+                return Ok(await categoryService.Update(id, category.Name));
             }
             catch(Exception)
             {
